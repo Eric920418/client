288422cbf6a9ffc0cfb17e26d80c582db180fa9a
eric <26416387.re@gmail.com>
2024-08-26 17:52:38 +0800

    test: 💍 ４５４４６４

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import ViteMonacoPlugin from 'vite-plugin-monaco-editor';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import ViteMonacoPlugin from "vite-plugin-monaco-editor";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/client/',
+  base: "/client/",
   plugins: [
     vue(),
     // MonacoEditorPlugin({ languages: ['javascript', 'typescript', 'html', 'css', 'json'] })
@@ -13,14 +13,19 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
-    host: '0.0.0.0',
-    port: 5173 // 确保端口是你想使用的端口
+    host: "0.0.0.0",
+    port: 5173, // 确保端口是你想使用的端口
   },
   optimizeDeps: {
-    exclude: ['pdfjs-dist']
-  }
+    exclude: ["pdfjs-dist"],
+  },
+  build: {
+    rollupOptions: {
+      external: ["vuetify"],
+    },
+  },
 });
