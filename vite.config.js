b3a4856168256e9da208c76eeec4b6304b9ed5c7
eric <26416387.re@gmail.com>
2024-09-27 10:22:55 +0800

    test: 💍 https

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import MonacoEditorPlugin from "vite-plugin-monaco-editor";
+import fs from "fs";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
